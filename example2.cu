9571f2cdb912b5fccae5fe4d6203fec53f996207
Carlos Planelles <planelles20@gmail.com>
2016-02-29 09:45:55 +0100

    arreglado bug en la iniciacion y representacion de las matrices y en el kernel

--- example2.cu
+++ example2.cu
@@ -8,8 +8,8 @@ __global__ void Multy2Matrix(int m, int n, int k, float* A, float* B, float* C)
   int Col = blockIdx.x*blockDim.x+threadIdx.x;
   if ((Row < n) && (Col < k)) {
     float Cvalue = 0.0;
-    for (int i = 0; i < n; ++i)  Cvalue += A[Row*n+i] * B[Col+i*m];
-    C[Row*n+Col] = Cvalue;
+    for (int i = 0; i < m; ++i)  Cvalue += A[Row*n+i] * B[Col+i*m];
+    C[Row*k+Col] = Cvalue;
   }
 }
 
@@ -22,9 +22,9 @@ int main(void)
   float *d_a, *d_b, *d_c;
   float *h_a, *h_b, *h_c;
 
-  const int N = 4;
+  const int N = 2;
   const int M = 5;
-  const int K = 4;
+  const int K = 30;
 
   size_t size_a = N * M * sizeof(float);
   size_t size_b = M * K * sizeof(float);
@@ -39,21 +39,23 @@ int main(void)
 
   for (int i=0; i<N; i++){
     for (int j=0; j<M; j++){
-      h_a[j*M+i] = i+j;
+      h_a[j*N+i] = i+j;
     }
   }
 
   for (int i=0; i<M; i++){
     for (int j=0; j<K; j++){
-      h_b[j*K+i] = (i<j ? j:i);
+      h_b[j*M+i] = (i<j ? j:i);
     }
   }
 
   cudaMemcpy(d_a, h_a, size_a, cudaMemcpyHostToDevice);
   cudaMemcpy(d_b, h_b, size_b, cudaMemcpyHostToDevice);
 
-  dim3 DimGrid((N-1)/16 + 1, (K-1)/16 + 1, 1);
-  dim3 DimBlock(16, 16, 1);
+  const int threads = 256;
+
+  dim3 DimGrid((N-1)/threads + 1, (K-1)/threads + 1, 1);
+  dim3 DimBlock(threads, threads, 1);
   Multy2Matrix <<< DimGrid, DimBlock >>> (M, N, K, d_a, d_b, d_c);
 
   cudaMemcpy(h_c, d_c, sizeof(float)*N*K, cudaMemcpyDeviceToHost);
@@ -61,7 +63,7 @@ int main(void)
   std::cout << "\nA matrix \n";
   for (int i=0; i<N; i++){
     for (int j=0; j<M; j++){
-      std::cout << h_a[j*M+i] << " ";
+      std::cout << h_a[j*N+i] << " ";
     }
     std::cout << "\n";
   }
@@ -69,7 +71,7 @@ int main(void)
   std::cout << "\nB matrix \n";
   for (int i=0; i<M; i++){
     for (int j=0; j<K; j++){
-      std::cout << h_b[j*K+i] << " ";
+      std::cout << h_b[j*M+i] << " ";
     }
     std::cout << "\n";
   }
@@ -77,7 +79,7 @@ int main(void)
   std::cout << "\nC matrix \n";
   for (int i=0; i<N; i++){
     for (int j=0; j<K; j++){
-      std::cout << h_c[j*K+i] << " ";
+      std::cout << h_c[j*N+i] << " ";
     }
     std::cout << "\n";
   }
@@ -85,7 +87,4 @@ int main(void)
   cudaFree(d_a);
   cudaFree(d_b);
   cudaFree(d_c);
-
-
-
 }
